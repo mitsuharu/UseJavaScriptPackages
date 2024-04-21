e450d1cedadb650868ece4f6521b5c47aeed97f0
Mitsuharu Emoto <mthr1982@gmail.com>
2024-04-21 17:37:30 +0900

    Initial Commit

---
+++ UseJavaScriptPackages/UseJavaScriptPackagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UseJavaScriptPackagesApp.swift
+//  UseJavaScriptPackages
+//
+//  Created by Mitsuharu Emoto on 2024/04/21.
+//
+
+import SwiftUI
+
+@main
+struct UseJavaScriptPackagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
